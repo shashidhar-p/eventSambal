c3d35a61f03c5d0371f217f6131e39b45826a2c1
cse3 <cse3@GIT-LAB1-50>
2019-10-09 11:51:07 +0530

    Test commit

---
+++ src/eventsambal/EventSambal.java
@@ -0,0 +1,16 @@
+package eventsambal;
+
+/**
+ *
+ * @author cse3
+ */
+public class EventSambal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
